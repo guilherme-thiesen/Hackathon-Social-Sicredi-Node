d4bdb8bf1b4931b844ddf03810fb1b35b2a479eb
Guilherme Thiesen <guilhermethiesen@gmail.com>
2019-11-23 06:09:07 -0300

    Adicionada rota GET de usuários

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
 
 import GeoController from "./app/controllers/GeoController";
+import UserController from "./app/controllers/UserController";
 import authMiddleware from "./app/middlewares/auth";
 
 const routes = new Router();
 
 /** Private routes (only with Auth) */
-routes.use(authMiddleware);
+// routes.use(authMiddleware);
 
+routes.get("/usuarios", UserController.index);
 routes.get("/escolas-proximas", GeoController.index);
 
 export default routes;
