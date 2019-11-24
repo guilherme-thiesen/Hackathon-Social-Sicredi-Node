50f4050e31f0c5934709ec09cdb20fe92cb49c56
Guilherme Thiesen <guilhermethiesen@gmail.com>
2019-11-24 03:20:26 -0300

    route / controller school_category

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,20 @@ import { Router } from "express";
 import GeoController from "./app/controllers/GeoController";
 import UserController from "./app/controllers/UserController";
 import SchoolController from "./app/controllers/SchoolController";
+import SchoolCategoryController from "./app/controllers/SchoolCategoryController";
 import authMiddleware from "./app/middlewares/auth";
 
 const routes = new Router();
 
 /** Private routes (only with Auth) */
 // routes.use(authMiddleware);
+
+// Rotas de Categorias de escolas
+routes.get("/schools-category", SchoolCategoryController.index);
+routes.post("/schools-category", SchoolCategoryController.store);
+routes.put("/schools-category/:id", SchoolCategoryController.update);
+routes.delete("/schools-category/:id", SchoolCategoryController.delete);
+
 routes.post("/geo", GeoController.index);
 routes.get("/users", UserController.index);
 routes.get("/near-schools", GeoController.index);
